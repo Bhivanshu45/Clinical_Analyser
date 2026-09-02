c7c1f7cdbe44881b824c1294cbeaaae8f3a675c4
Bhivanshu45 <bhivanshulawaniya6@gmail.com>
2026-09-02 16:12:38 +0530

    Configure CORS for deployed frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    aallow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://clinical-analyser-phi.vercel.app",
+    ],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
